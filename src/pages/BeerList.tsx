8d1f8f2c58ff0cb0a245de6d5a393b450eb5b4c6
Ирина Черняева <iridache@192.168.1.2>
2023-04-06 14:59:48 +0300

    pagination done

--- src/pages/BeerList.tsx
+++ src/pages/BeerList.tsx
@@ -1,63 +1,42 @@
 import {
-  IonContent,
   IonHeader,
   IonImg,
   IonList,
-  IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import { useState, useEffect } from "react";
-import Pagination from "../components/Pagination";
-import { restApiService } from "../utils/RestApiService";
 import { BeerItem } from "../utils/types";
 import styles from "./BeerList.module.css";
 
+export interface BeerListProps {
+  beers: BeerItem[];
+}
 
-const BeerList = () => {
-  const [data, setData] = useState<BeerItem[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [recordsPerPage] = useState(5);
-
-  useEffect(() => {
-    restApiService.getBeers().then((res) => setData(res));
-  }, []);
-  
-  const indexOfLastRecord = currentPage * recordsPerPage;
-  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
-  const currentRecords = data.slice(indexOfFirstRecord, indexOfLastRecord);
-  const nPages = Math.ceil(data.length / recordsPerPage);
+const BeerList = ({ beers }: BeerListProps) => {
 
   return (
-    <IonPage>
+    <>
       <IonHeader>
         <IonToolbar>
           <IonTitle className={styles.heading}>Beer Menu</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonList className={styles.list} inset={true}>
-          {currentRecords &&
-            currentRecords.map((beerItem) => {
-              return (
-                <div className={styles.card} key={beerItem.id}>
-                  <IonImg
-                    className={styles.beerImg}
-                    src={beerItem.image_url}
-                    alt={beerItem.name}
-                  ></IonImg>
-                  <div className={styles.abv}>ABV: {beerItem.abv}%</div>
-                </div>
-              );
-            })}
-        </IonList>
-        <Pagination
-              nPages={nPages}
-              currentPage={currentPage}
-              setCurrentPage={setCurrentPage}
-            />
-      </IonContent>
-    </IonPage>
+      <IonList className={styles.list} inset={true}>
+        {beers &&
+          beers.map((beerItem) => {
+            return (
+              <div className={styles.card} key={beerItem.id}>
+                <IonImg
+                  className={styles.beerImg}
+                  src={beerItem.image_url}
+                  alt={beerItem.name}
+                ></IonImg>
+                <div className={styles.abv}>ABV: {beerItem.abv}%</div>
+              </div>
+            );
+          })}
+      </IonList>
+    </>
   );
 };
 
