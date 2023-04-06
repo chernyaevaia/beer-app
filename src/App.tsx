fd2c5ad419ce23e159c0b4af25f6578b8dd7fdea
Ирина Черняева <iridache@192.168.1.2>
2023-04-06 15:01:25 +0300

    pagination done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { Redirect, Route } from "react-router-dom";
-import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import {
+  IonApp,
+  IonContent,
+  IonPage,
+  IonRouterOutlet,
+  setupIonicReact,
+} from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import Home from "./pages/Home";
+import BeerList from "./pages/BeerList";
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
 
@@ -20,22 +26,51 @@ import "@ionic/react/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
+import Pagination from "./components/Pagination";
+import { useState, useEffect } from "react";
+import { restApiService } from "./utils/RestApiService";
+import { BeerItem } from "./utils/types";
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App = () => {
+  const [data, setData] = useState<BeerItem[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage] = useState(5);
+
+  useEffect(() => {
+    restApiService.getBeers().then((res) => setData(res));
+  }, []);
+
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  const currentRecords = data.slice(indexOfFirstRecord, indexOfLastRecord);
+  const nPages = Math.ceil(data.length / recordsPerPage);
+
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/home">
+            <IonPage>
+              <IonContent fullscreen>
+                <BeerList beers={currentRecords} />
+                <Pagination
+                  nPages={nPages}
+                  currentPage={currentPage}
+                  setCurrentPage={setCurrentPage}
+                />
+              </IonContent>
+            </IonPage>
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,53 @@
+import styles from "./Pagination.module.css";
+
+export interface PaginationProps {
+  nPages: number;
+  currentPage: number;
+  setCurrentPage: any;
+}
+
+const Pagination = ({
+  nPages,
+  currentPage,
+  setCurrentPage,
+}: PaginationProps) => {
+  const pageNumbers = [...Array(nPages + 1).keys()].slice(1);
+
+  const nextPage = () => {
+    if (currentPage !== nPages) setCurrentPage(currentPage + 1);
+  };
+  const prevPage = () => {
+    if (currentPage !== 1) setCurrentPage(currentPage - 1);
+  };
+
+  return (
+    <nav className={styles.pagination}>
+      <ul className={styles.list}>
+        <li>
+          <a className={styles.pageNum} onClick={prevPage}>
+            Previous
+          </a>
+        </li>
+        {pageNumbers.map((pgNumber) => (
+          <li key={pgNumber}>
+            <a
+              onClick={() => setCurrentPage(pgNumber)}
+              className={
+                currentPage == pgNumber ? styles.pageNumActive : styles.pageNum
+              }
+            >
+              {pgNumber}
+            </a>
+          </li>
+        ))}
+        <li>
+          <a onClick={nextPage} className={styles.pageNum}>
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
